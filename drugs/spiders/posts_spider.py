c7042204944c0f3d9f5aa1e66e90ffcfae09c972
kareem <kareemels1996@gmail.com>
2021-06-27 04:06:27 +0200

    check for name retail existance

--- drugs/spiders/posts_spider.py
+++ drugs/spiders/posts_spider.py
@@ -18,10 +18,13 @@ class PostsSpider(scrapy.Spider):
             yield from response.follow_all(next_page, callback=self.parse)
            
     def parseDrugItem(self, response):
+        name_retail = response.css('div.even::text')[1].get()
+        if name_retail is None:
+            name_retail = '';
         yield {
             'img': response.css('img.img-fluid::attr(src)').get(),
             'name': response.css('div.mid-snipp h1::text').get(),
-            'name_retail': response.css('div.even::text')[1].get(),
+            'name_retail': name_retail,
             'name_scientific': response.css('div.odd::text')[3].get(),
             'price': response.css('p.m-price::text').get(),
             'desc_ar': response.css('div#arabic-desc::text').getall(),
